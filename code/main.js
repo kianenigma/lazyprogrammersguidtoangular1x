e5fdd4ca9056e11478b5625df2ba01a2e5f458f7
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2016-08-01 22:48:44 +0430

    Code #2 directive introduction

--- code/main.js
+++ code/main.js
@@ -1,13 +1,41 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainCtrl', function($scope, $interval){
+myApp.directive('titleEditor', function() {
+  return {
+    restrict: 'AE', // how this directive can be used. <attribute> and <element> in this case
+    scope: {
+      'parentTitle': '=title'
+    }, // isolated scope
+    template: '<h1> I Am poped inside from a directive! </h1>' +
+      ' <p>{{ directiveData }}</p>' +
+      '<p> i am $parent.Title : {{ $parent.Title }} </p>' +
+      '<p> i am parentTitle : {{ parentTitle }} </p>' + 
+      '<input ng-model="parentTitle" type="text" />' ,
+    controller: function($scope) {
+        $scope.directiveData = "I Live inside the directive!";
+        console.log($scope.$parent.Title);
+        console.log($scope.parentTitle);
+      }, // custom controller for this directive
+    link: function(scope, iElem, iAttr, ctrl) {
+      iElem.on('click', function(){
+        console.log("clicked!");
+      })
+      console.log(scope, iElem, iAttr, ctrl);
+    }
+  }
+})
+
+myApp.controller('mainCtrl', function($scope, $interval) {
   $scope.Title = 'Hello Angualr from data binding';
+  $scope.names = ["Marshall", "Ted", "Barney", "Robin"];
 
   $scope.alert = function() {
     alert("Yay!");
   }
 
-  $interval(function() {$scope.Title = 'Hello Angualr from data binding at ' + new Date()})
+  $scope.onListItemClick = function(name) {
+    alert(name);
+  }
 
   $scope.title = function() {
     return "Hello form a function";
