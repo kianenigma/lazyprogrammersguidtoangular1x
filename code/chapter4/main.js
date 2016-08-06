74ee37a7abe07f3e35021430cf9b08c863df1677
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2016-08-06 18:38:28 +0430

    chapter 4 part 1 finished

--- code/chapter4/main.js
+++ code/chapter4/main.js
@@ -3,10 +3,23 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('mainCtrl', function ($scope) {
   $scope.Title = 'Chapter 4 starts now!';
-  $scope.actors;
+  $scope.actors = {
+    "Ted": {
+      lastName: "Mosbey",
+      age: 27,
+      bars: ["Blind Tiger", "Torst", "Good Beer"]
+    },
+    "Barney": {
+      lastName: "Stinson",
+      age: 30,
+      bars: ["The Poney Bar"]
+    }
+  }
 
   $scope.sayHi = function () {
-    alert('hello there')
+    alert('hello there');
   }
+
+
 
 });
